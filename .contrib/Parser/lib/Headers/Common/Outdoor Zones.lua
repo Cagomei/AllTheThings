e2a1fa3b8dcf6b18b7eb93b7770fc4c24cc7850a
ImUnicke <ImUnicke+github@gmail.com>
2026-02-26 10:20:43 -0700

    [Logic] Ignore 'Outdoor Zones' header in minilist since it's typically redundant with 'Remote Zones' for any mapped content

--- .contrib/Parser/lib/Headers/Common/Outdoor Zones.lua
+++ .contrib/Parser/lib/Headers/Common/Outdoor Zones.lua
@@ -2,6 +2,7 @@ OUTDOOR_ZONES = createHeader({
 	readable = "Outdoor Zones",
 	icon = [[~_.asset("Category_Zones")]],
 	constant = "OUTDOOR_ZONES",
+	minilist_ignore = true,
 	text = {
 		en = [[~BUG_CATEGORY2]],
 	},
